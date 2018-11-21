de965c2eaf0c5e589e5375658c0c09827c760273
David Moranchel <david.arcshtag@gmail.com>
2018-11-21 04:15:54 -0600

    Section component data

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { PostRef } from '../../fire';
+import { PostRef, SectionRef } from '../../fire';
 // styles
 import './Sections.css';
 // icons
@@ -9,16 +9,44 @@ class Sections extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sections: []
+      section: [],
+      posts: []
     }
   }
 
   componentDidMount() {
     let section = this.props.match.params.section;
-    console.log(section);
+
+    this.handlerSection = SectionRef.orderByChild('key').equalTo(section);
+    this.handlerSection.on('value', snapshot => {
+      let sectionArray = [];
+      snapshot.forEach( snap => {
+        let { name, key, img } = snap.val();
+        sectionArray.push({ name, key, img, id: snap.key });
+      });
+      this.setState({ section: sectionArray });
+    });
+
+    this.handlerPosts = PostRef.orderByChild('section').equalTo(section);
+    this.handlerPosts.on('value', snapshot => {
+      let postArray = [];
+      snapshot.forEach( snap => {
+        let { title, urlImg, description, username } = snap.val();
+        postArray.push({
+          title,
+          urlImg,
+          description,
+          username,
+          key: snap.key
+        });
+      });
+      this.setState({ posts: postArray });
+    });
   }
 
   render() {
+    console.log(this.state.posts);
+    console.log(this.state.section);
     return(
       <h1>{ this.props.match.params.section }</h1>
     )
